66e1082395e1005af0f8beea62aa4b704620cdae
杨东浩 <yangdonghao@buaa.edu.cn>
2025-05-31 14:47:25 +0800

    little change

--- scripts/oscat-wo.bat
+++ scripts/oscat-wo.bat
@@ -1,9 +1,9 @@
-& d:/autoPLCASEproject/gitAutoPLC/AutoPLC/venv/Scripts/python.exe d:/autoPLCASEproject/gitAutoPLC/AutoPLC/src/autoplc/main_st.py --benchmark oscat --config default
+python src/autoplc/main_st.py --benchmark oscat --config default
 
-& d:/autoPLCASEproject/gitAutoPLC/AutoPLC/venv/Scripts/python.exe d:/autoPLCASEproject/gitAutoPLC/AutoPLC/src/autoplc/main_st.py --benchmark oscat --config wo-apirec-st
+python src/autoplc/main_st.py --benchmark oscat --config wo-apirec-st
 
-& d:/autoPLCASEproject/gitAutoPLC/AutoPLC/venv/Scripts/python.exe d:/autoPLCASEproject/gitAutoPLC/AutoPLC/src/autoplc/main_st.py --benchmark oscat --config wo-debugger-st
+python src/autoplc/main_st.py --benchmark oscat --config wo-debugger-st
 
-& d:/autoPLCASEproject/gitAutoPLC/AutoPLC/venv/Scripts/python.exe d:/autoPLCASEproject/gitAutoPLC/AutoPLC/src/autoplc/main_st.py --benchmark oscat --config wo-planner-st
+python src/autoplc/main_st.py --benchmark oscat --config wo-planner-st
 
-& d:/autoPLCASEproject/gitAutoPLC/AutoPLC/venv/Scripts/python.exe d:/autoPLCASEproject/gitAutoPLC/AutoPLC/src/autoplc/main_st.py --benchmark oscat --config wo-retrival-st
+python src/autoplc/main_st.py --benchmark oscat --config wo-retrival-st
